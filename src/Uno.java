3de829a644d7931e0ca9d0fc133b92366244b35a
Nicolas Mak-Fan <33139217+makfni@users.noreply.github.com>
2019-03-30 03:15:43 -0400

    Added comments to Uno class
    This will help me get an idea of how to go about the logic of the game and how make the plays abide by the rules.

--- src/Uno.java
+++ src/Uno.java
@@ -1,6 +1,24 @@
-
-
 public class Uno{
+//All game logic will reside in this class
+
+    //startGame function to initialize hands
+
+    //check for winner (empty hand)
 
 
+    //make each player play their turn
+    //player goes first and plays game on console
+    //AI will play by having their hand looped through to check for playable cards
+    //if they receive an ability card, they will play it right away
+    //check if deck is empty (if it's not, then next player goes)
+    //if no play can be made, draw card from deck if deck is not empty
+    //else, call reuse pile function
+
+
+
+    //check for empty deck (if it is then reuse pile)
+
+    //invalid move function for main player?
+
+    //check for winner function
 }
